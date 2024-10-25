b19725d5bec5f6b2d6f35ee3de91674011527780
BaseCheesePunk <176217759+BaseCheesePunk@users.noreply.github.com>
2024-10-25 18:04:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,65 @@ mintButton.addEventListener("click", () => {
     alert("Minting limit reached!");
   }
 });
+
+import { createThirdwebClient } from "thirdweb";
+import { ConnectButton } from "thirdweb/react";
+import { darkTheme } from "thirdweb/react";
+import { createWallet } from "thirdweb/wallets";
+import { ethereum } from "thirdweb/chains";
+
+const client = createThirdwebClient({
+  clientId: "....",
+});
+
+const wallets = [
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  createWallet("me.rainbow"),
+  createWallet("io.rabby"),
+  createWallet("io.zerion.wallet"),
+  createWallet("com.okex.wallet"),
+  createWallet("com.bitget.web3"),
+  createWallet("com.binance"),
+  createWallet("com.bybit"),
+  createWallet("com.trustwallet.app"),
+  createWallet("org.uniswap"),
+  createWallet("com.ledger"),
+  createWallet("pro.tokenpocket"),
+  createWallet("com.safepal"),
+];
+
+function Example() {
+  return (
+    <ConnectButton
+      client={client}
+      wallets={wallets}
+      theme={darkTheme({
+        colors: { accentText: "#0000ff" },
+      })}
+      connectButton={{ label: "Connect Wallet" }}
+      connectModal={{
+        size: "compact",
+        title: "Connect Wallet",
+      }}
+      accountAbstraction={{
+        chain: ethereum, // replace with the chain you want
+        sponsorGas: true,
+      }}
+      auth={{
+        async doLogin(params) {
+          // call your backend to verify the signed payload passed in params
+        },
+        async doLogout() {
+          // call your backend to logout the user if needed
+        },
+        async getLoginPayload(params) {
+          // call your backend and return the payload
+        },
+        async isLoggedIn() {
+          // call your backend to check if the user is logged in
+        },
+      }}
+    />
+  );
+}
